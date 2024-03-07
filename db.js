dda7d3900d6aa8dcec9e0cf093dd24a3faf393b2
kajalm <kajal.madhusudan03@gmail.com>
2024-03-07 13:35:53 +0530

    Update db.js

--- db.js
+++ db.js
@@ -10,11 +10,24 @@
 
 // export default pool;
 
+// import { Pool } from 'pg';
+
+// const pool = new Pool({
+//   user: 'postgres',
+//   password: 'aditya',
+//   host: 'localhost',
+//   port: 5432,
+//   database: 'hh',
+// });
+
+// export default pool;
+
+
 import { Pool } from 'pg';
 
 const pool = new Pool({
   user: 'postgres',
-  password: 'aditya',
+  password: 'root',
   host: 'localhost',
   port: 5432,
   database: 'hh',
