e807161a4fab107a55a51c5ea08fba2cf2eb33e1
Jyotishman Shandilya <jyotishmanshandilyaggn@gmail.com>
2024-03-11 18:18:17 +0530

    Fully working codebase

--- app/checkout/page.js
+++ app/checkout/page.js
@@ -138,7 +138,7 @@
                 <div className="mt-6">
                   <button
                     onClick={handlePlaceOrder}
-                    className="flex justify-center w-full px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 md:py-4 md:text-lg md:px-10"
+                    className="flex justify-center w-half px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 md:py-4 md:text-lg md:px-10 bg-black"
                   >
                     Place order
                   </button>
