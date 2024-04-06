63d0d2f6bb5a876377c8cdee6747cb3d4dd85ce6
Jyotishman Shandilya <jyotishmanshandilyaggn@gmail.com>
2024-04-06 19:23:14 +0530

    minor changes

--- app/auth/login/page.js
+++ app/auth/login/page.js
@@ -1,7 +1,7 @@
  'use client'
 import { useState } from 'react';
 import { useRouter } from 'next/navigation';
-import backgroundImage from 'public/Bg.jpg';
+// import backgroundImage from 'public/Bg.jpg';
 
 const Login = () => {
   const router = useRouter();
